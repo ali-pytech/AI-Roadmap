ee161758c8aede46b31c0ccc80e119b0050cc255
Ali Hamza <ali.pytech@gmail.com>
2026-01-30 09:31:05 +0500

    Housing.py

--- Day10/Housing.py
+++ Day10/Housing.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     try:
         monthly_rent = int(input("Enter your monthly rent: "))
         total_rent = annual_rent(monthly_rent)
-        print(f"Annual rent for {monthly_rent} SAR per month will be {total_rent} SAR")
+        print(f"Your Monthly Rent is {monthly_rent} SAR so Annual Rent will be {total_rent} SAR")
     except ValueError:
         print("Invalid input. Please enter numbers only!")
