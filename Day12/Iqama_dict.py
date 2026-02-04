52c2706962566e96bfd1ff3b5f0a078a1abd3373
Ali Hamza <ali.pytech@gmail.com>
2026-02-04 13:13:12 +0500

    Iqama_dict.py
    Add iqama renewal dictionary with year and fee records

---
+++ Day12/Iqama_dict.py
@@ -0,0 +1,10 @@
+"""Iqama Dictionary"""
+iqama = {
+    "Ali": {"year":2026, "fee": 800},
+    "Ahmed": {"year":2025, "fee":750},
+    "Faisal": {"year":2024, "fee":600}
+
+}
+
+for person,details in iqama.items():
+    print(f"{person} - Renewal Year: {details["year"]} and Fee is {details["fee"]} SAR")
