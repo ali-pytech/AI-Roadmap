d89bab3213b3ede37721584ad1e147eff18d9382
Ali Hamza <ali.pytech@gmail.com>
2026-02-10 12:48:02 +0500

    Grades_dict.py

--- Day12/grades_dict.py
+++ Day12/grades_dict.py
@@ -8,4 +8,4 @@ grades = {
 Average = sum(grades.values()) / len(grades)
 highest_student = max(grades, key=grades.get)
 print(f"Everage Garde: {Average}")
-print(f"Highest Scorer: {highest_student} with {grades[highest_student]} Marks")
+print(f"Highest Scorer: {highest_student} with {grades[highest_student]} Marks") 
