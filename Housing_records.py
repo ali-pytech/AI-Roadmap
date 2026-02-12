8c6987e3d1b7c7e0c17e16631d842a2cec438025
Ali Hamza <ali.pytech@gmail.com>
2026-02-12 14:33:01 +0500

    Create Housing_records.py
    Add housing records with rent, location, and size

---
+++ Housing_records.py
@@ -0,0 +1,9 @@
+""Apartment Record"""
+Apartments = {
+    "Apartment A" : {"monthly_rent": 2300, "Location": "Riyadh", "Size": "2 Bedroom"},
+    "Apartment B" : {"monthly_rent": 2100, "Location": "Jeddah", "Size": "3 Bedroom"},
+    "Apartment C" : {"monthly_rent": 1800, "Location": "Dammam", "Size": "Studio"}
+}
+for apartment , details in Apartments.items():
+    annual_rent = details['monthly_rent'] * 12
+    print(f"{apartment} Monthly Rent: {details['monthly_rent']} SAR Location: {details['Location']} Annual Rent: {annual_rent} SAR")
