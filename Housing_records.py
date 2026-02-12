556cb727f012ed3ee63a3825db8913f72ddc7bb7
Ali Hamza <ali.pytech@gmail.com>
2026-02-12 14:33:38 +0500

    Add housing records with rent, location, and size

--- Housing_records.py
+++ Housing_records.py
@@ -7,3 +7,4 @@ Apartments = {
 for apartment , details in Apartments.items():
     annual_rent = details['monthly_rent'] * 12
     print(f"{apartment} Monthly Rent: {details['monthly_rent']} SAR Location: {details['Location']} Annual Rent: {annual_rent} SAR")
+ 
