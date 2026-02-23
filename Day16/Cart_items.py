fbc4748da6211c6d696b8a19f600d102fa023247
Ali Hamza <ali.pytech@gmail.com>
2026-02-23 14:50:20 +0500

    Check product presence in shopping cart using membership

---
+++ Day16/Cart_items.py
@@ -0,0 +1,8 @@
+"""Shopping Cart Checker"""
+cart_items = {"shoes","T-shirt","watch","jeans"}
+
+if "T-shirt" in cart_items:
+    print("Yes, T-Shirt is in Cart ")
+
+else:
+    print("No, T-Shirt is not in your Cart")
