53c9ea92a229816f4a95c3f938402199c1afab03
Ali Hamza <ali.pytech@gmail.com>
2026-02-23 14:47:56 +0500

    Identify absent students using set difference

---
+++ Day16/Student_difference.py
@@ -0,0 +1,6 @@
+
+"""Student Difference"""
+Registered_students = {"Ali","Ahmed","Bilal","Faisal", "Umer"}
+Attended_students = {"Ali","Umer","Bilal"}
+Absent_students = Registered_students -  Attended_students
+print(f"Absent Students: {Absent_students}")
