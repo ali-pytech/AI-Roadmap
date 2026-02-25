fa724be29a3f47cb32a81a6d2df5e5dfbbf73457
Ali Hamza <ali.pytech@gmail.com>
2026-02-25 08:01:45 +0500

    Define function to evaluate student grade status

---
+++ Day17/Student_grade.py
@@ -0,0 +1,6 @@
+"""Housing Annual Rent"""
+def Annual_rent(monthly_rent):
+    return monthly_rent * 12
+
+print(f"Apartment A Annual Rent = {Annual_rent(1500)} SAR")
+print(f"Apartment B Annual Rent = {Annual_rent(2000)} SAR")
